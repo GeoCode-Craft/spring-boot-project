6d572ac28c8ec4ddc4b1e7c741a19c95d9a256c5
PondiB <pondibrian@gmail.com>
2020-08-17 16:49:50 +0300

    :sparkles: checking if user exists and utils

--- src/main/java/com/brianpondi/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/brianpondi/app/ws/service/impl/UserServiceImpl.java
@@ -1,8 +1,9 @@
 package com.brianpondi.app.ws.service.impl;
 
-import com.brianpondi.app.ws.repository.UserRepository;
 import com.brianpondi.app.ws.io.entity.UserEntity;
+import com.brianpondi.app.ws.repository.UserRepository;
 import com.brianpondi.app.ws.service.UserService;
+import com.brianpondi.app.ws.shared.Utils;
 import com.brianpondi.app.ws.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,24 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Utils utils;
+
     @Override
     public UserDto createUser(UserDto user) {
+
+
+        if(userRepository.findByEmail(user.getEmail()) != null) {
+            throw new RuntimeException("Record already exists");
+        }
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        String publicUserId = utils.generateUserId(30);
+        userEntity.setUserId(publicUserId);
         userEntity.setEncryptedPassword("test");
-        userEntity.setUserId("testUserId");
+
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
