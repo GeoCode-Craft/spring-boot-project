6ae7ac1c0b6ae7468b965c559089a714b6f27fc3
PondiB <pondibrian@gmail.com>
2020-09-02 11:55:22 +0300

    :art: paging and limit implementation

--- src/main/java/com/brianpondi/app/ws/ui/controller/UserController.java
+++ src/main/java/com/brianpondi/app/ws/ui/controller/UserController.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/users") //http://localhost:8081/users
 public class UserController {
@@ -73,6 +76,22 @@ public class UserController {
         userService.deleteUser(id);
 
         returnValue.setOperationResult(RequestOperationStatus.SUCCESS.name());
+        return returnValue;
+    }
+    //paging:
+    //http://localhost:8081/users?page=1&limit=30
+    @GetMapping( produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public List<UserRest> getUsers(@RequestParam(value = "page",defaultValue ="0")int page,
+                                   @RequestParam(value = "limit",defaultValue ="25") int limit){
+        List<UserRest> returnValue = new ArrayList<>();
+
+        List<UserDto> users = userService.getUsers(page, limit);
+
+        for(UserDto userDto: users){
+            UserRest userModel = new UserRest();
+            BeanUtils.copyProperties(userDto, userModel);
+            returnValue.add(userModel);
+        }
         return returnValue;
     }
 
