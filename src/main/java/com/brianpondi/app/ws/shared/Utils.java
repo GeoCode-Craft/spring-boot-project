e17d8aaf5602c44625dd3d5fbe153bef3644ad24
PondiB <pondibrian@gmail.com>
2020-09-04 15:15:08 +0300

    :key: Use utils to generate addressId for the public

--- src/main/java/com/brianpondi/app/ws/shared/Utils.java
+++ src/main/java/com/brianpondi/app/ws/shared/Utils.java
@@ -9,20 +9,23 @@ import java.util.Random;
 public class Utils {
 
     private final Random RANDOM = new SecureRandom();
-    private final String ALPHABET = "0123456789ABCDEFGHIKLMNOPQRSTVXYZabcdefghijklmnopqrstuvwxyz";
-    //private final int ITERATIONS = 10_000;
-   // private final int KEY_LENGTH = 256;
+    private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    public String generateUserId(int length){
+    public String generateUserId(int length) {
         return generateRandomString(length);
     }
 
-    private String generateRandomString(int length){
+    public String generateAddressId(int length) {
+        return generateRandomString(length);
+    }
+
+    private String generateRandomString(int length) {
         StringBuilder returnValue = new StringBuilder(length);
 
-        for(int i = 0; i<length; i++){
+        for (int i = 0; i < length; i++) {
             returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
         }
+
         return new String(returnValue);
     }
 }
