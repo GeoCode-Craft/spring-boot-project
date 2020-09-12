3b2e275af3244217dbaaf7a399da4bfdcdab7e19
PondiB <pondibrian@gmail.com>
2020-09-13 00:22:54 +0300

    :art: Spring HATEOAS new way

--- src/main/java/com/brianpondi/app/ws/ui/controller/UserController.java
+++ src/main/java/com/brianpondi/app/ws/ui/controller/UserController.java
@@ -11,11 +11,18 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/users") //http://localhost:8081/mobile-app-ws/users
@@ -111,29 +118,54 @@ public class UserController {
     //http://localhost:8081/mobile-app-ws/users/hdhss745554934/addresses
     @GetMapping(path="/{id}/addresses",
             produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public List<AddressesRest> getUserAddresses(@PathVariable String  id)
-    {
-        List<AddressesRest> returnValue = new ArrayList<>();
+    public CollectionModel<AddressesRest> getUserAddresses(@PathVariable String id) {
+        List<AddressesRest> addressesListRestModel = new ArrayList<>();
+
         List<AddressDto> addressesDto = addressesService.getAddresses(id);
 
-        if(addressesDto != null && !addressesDto.isEmpty()) {
-            java.lang.reflect.Type listType = new TypeToken<List<AddressesRest>>() {}.getType();
-            ModelMapper modelMapper = new ModelMapper();
-            returnValue = modelMapper.map(addressesDto, listType);
+        Link userLink = null;
+
+        if (addressesDto != null && !addressesDto.isEmpty()) {
+            Type listType = new TypeToken<List<AddressesRest>>() {
+            }.getType();
+            addressesListRestModel = new ModelMapper().map(addressesDto, listType);
+
+            for (AddressesRest addressRest : addressesListRestModel) {
+                Link addressLink = linkTo(methodOn(UserController.class).getUserAddress(id, addressRest.getAddressId()))
+                        .withSelfRel();
+                addressRest.add(addressLink);
+
+                userLink = linkTo(methodOn(UserController.class).getUser(id)).withRel("user");
+                addressRest.add(userLink);
+            }
+
         }
+         CollectionModel<AddressesRest> returnValue = new CollectionModel<>(addressesListRestModel, userLink);
+
         return returnValue;
     }
+
 
     //Getting single addresses with address id:
     //http://localhost:8081/mobile-app-ws/users/hdhss745554934/addresses/ghggsd8888
     @GetMapping(path="/{userId}/addresses/{addressId}",
             produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public AddressesRest getUserAddress(@PathVariable String  addressId){
-        AddressDto addressDto = addressService.getAddress(addressId);
+    public EntityModel<AddressesRest> getUserAddress(@PathVariable String userId, @PathVariable String addressId){
+
+        AddressDto addressesDto = addressService.getAddress(addressId);
 
         ModelMapper modelMapper = new ModelMapper();
+        Link addressLink = linkTo(methodOn(UserController.class).getUserAddress(userId, addressId)).withSelfRel();
+        Link userLink = linkTo(UserController.class).slash(userId).withRel("user");
+        Link addressesLink = linkTo(methodOn(UserController.class).getUserAddresses(userId)).withRel("addresses");
 
-        return  modelMapper.map(addressDto, AddressesRest.class);
+        AddressesRest addressesRestModel = modelMapper.map(addressesDto, AddressesRest.class);
+
+        addressesRestModel.add(addressLink);
+        addressesRestModel.add(userLink);
+        addressesRestModel.add(addressesLink);
+
+        return new EntityModel<>(addressesRestModel);
     }
 
 }
